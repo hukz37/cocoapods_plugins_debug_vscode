828121f6fec7ea6bc3543fafb18da464a055b52c
hu <595632239@163.com>
2022-10-04 01:13:27 +0800

    feat: init plugin

--- cocoapods-ddplugin/lib/cocoapods_plugin.rb
+++ cocoapods-ddplugin/lib/cocoapods_plugin.rb
@@ -1,1 +1,8 @@
-require 'cocoapods-ddplugin/command'
+# !/usr/bin/env ruby
+
+
+module DDPluginHeaderMap
+  Pod::HooksManager.register('cocoapods-ddplugin', :post_install) do |post_context|
+    puts '进入了自定义hook'
+  end
+end
